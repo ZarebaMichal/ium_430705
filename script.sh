31492ed478e0e3eba64f01e2f31dc7ddd0e19328
michalzareba <michal.zareba@synapsi.xyz>
2021-03-28 21:19:20 +0200

    fix script

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -LO https://git.wmi.amu.edu.pl/s430705/ium_430705/raw/branch/master/imdb_movies.csv
-shuf ./imdb_movies.csv > ./imdb_movies2.csv | tail -n +5 > ./imdb_movies2.csv
+shuf ./imdb_movies.csv > ./imdb_movies2.csv | tail -n +$((${CUTOFF})) > ./imdb_movies2.csv
 wc -l ./imdb_movies.csv
 head -n 4600 ./imdb_movies2.csv > ./test.csv
 head -n 4600 ./imdb_movies2.csv | tail -n 4600 > ./dev.csv
