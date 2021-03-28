e54a40db7d4ddc212a109952031e8f96be971310
michalzareba <michal.zareba@synapsi.xyz>
2021-03-28 20:24:44 +0200

    fix shell script

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-curl -OL https://git.wmi.amu.edu.pl/s430705/ium_430705/src/branch/master/imdb_movies.csv
+curl -LO https://git.wmi.amu.edu.pl/s430705/ium_430705/raw/branch/master/imdb_movies.csv
 head -n -1 imdb_movies.csv | shuf > imdb_movies.csv.shuf
 wc -l imdb_movies.csv
-head -n 126 imdb_movies.csv.shuf > test.csv
-head -n 126 imdb_movies.csv.shuf | tail -n 2006 > dev.csv
-tail -n 379 imdb_movies.csv.shuf > train.csv
+head -n 4600 imdb_movies.csv.shuf > test.csv
+head -n 4600 imdb_movies.csv.shuf | tail -n 4600 > dev.csv
+tail -n 13800 imdb_movies.csv.shuf > train.csv
 wc -l *.csv
