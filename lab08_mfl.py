b7556dedb02b58eb5e625e463aecb797c50d6328
s430705 <miczar1@st.amu.edu.pl>
2021-05-12 09:42:24 +0200

    Split sacred and MLflow to separate files

--- lab08_mfl.py
+++ lab08_mfl.py
@@ -2,41 +2,20 @@ import sys
 import mlflow
 import pandas as pd
 
-from sacred import Experiment
-from sacred.observers import FileStorageObserver
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.models import Sequential
 
-'''
-If you want to use it as sacred, you have to 
-uncomment lines, for now it's set up for MLFlow
-'''
-# ex = Experiment("file_observer", interactive=False, save_git_info=False)
-# ex.observers.append(FileStorageObserver('lab07/my_runs'))
 
-
-# @ex.config
-# def my_config():
-#     train_size_param = 0.8
-#     test_size_param = 0.2
-#     epochs = 400
-#     batch_size = 128
-
-
-# @ex.capture
-def prepare_model(train_size_param, test_size_param, epochs, batch_size):  # _run):
-    # _run.info["prepare_model_ts"] = str(datetime.now())
+def prepare_model(train_size_param, test_size_param, epochs, batch_size):
     movies_data = pd.read_csv("train.csv", error_bad_lines=False)
     movies_data.drop(movies_data.columns[0], axis=1, inplace=True)
     movies_data.dropna(inplace=True)
     X = movies_data.drop("rating", axis=1)
     Y = movies_data["rating"]
 
-    print(X, Y.values)
-    # Split set to train/test 8:2 ratio
     X_train, X_test, Y_train, Y_test = train_test_split(
         X, Y, test_size=test_size_param, random_state=42
     )
@@ -69,20 +48,9 @@ def prepare_model(train_size_param, test_size_param, epochs, batch_size):  # _ru
 
     rmse = mean_squared_error(y_test, y_pred)
 
-    # _run.info["Final Results: "] = rmse
-
     model.save("model_movies")
 
     return model, rmse
-
-
-# @ex.automain
-# def my_main(train_size_param, test_size_param, epochs, batch_size):
-# print(prepare_model())
-
-
-# r = ex.run()
-# ex.add_artifact("model_movies/saved_model.pb")
 
 
 train_size_param = float(sys.argv[1]) if len(sys.argv) > 1 else 0.8
